aa98dd55513b70f665fd4fa1b54ce1388c14ec64
Geraldo Ramos <gramos1984@gmail.com>
2013-12-19 20:09:38 -0500

    Update maps

--- assets/map/home.lua
+++ assets/map/home.lua
@@ -190,19 +190,6 @@ return {
           properties = {
             ["from"] = "west"
           }
-        },
-        {
-          name = "Chest",
-          type = "",
-          shape = "rectangle",
-          x = 640,
-          y = 385,
-          width = 32,
-          height = 32,
-          visible = true,
-          properties = {
-            ["id"] = "1"
-          }
         }
       }
     }
